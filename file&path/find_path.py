69a521098a198a9833bb653f380a46e3dcd94fff
Dou Liyou <douliyou@live.cn>
2019-03-01 08:51:25 +0800

    修改逻辑，增加注释

--- file&path/find_path.py
+++ file&path/find_path.py
@@ -17,8 +17,10 @@ def find_path(src_dir,spec_path):
         for fn in listdir(current_dir):
             if fn == spec_path:
                 return join(current_dir,spec_path)
-            if fn != "temprepo" or fn != ".svn" or fn != ".git":
+            # 必须不是 temprepo, .svn 和 .git 这三种文件才进行处理
+            if fn != "temprepo" and fn != ".svn" and fn != ".git":
                 path = join(current_dir,fn)
+                print path.decode('utf-8').encode('gbk')
             if isfile(path):
                 continue
             dirs.append(path)
@@ -27,5 +29,5 @@ def find_path(src_dir,spec_path):
 if __name__ == "__main__":
     # decode 和 encode 因为在nodepad++控制台显示乱码才加的，在cmd下直接运行显示正常
     print find_path(u"E:\\迁移公司库",u"啊").decode('utf-8').encode('gbk')
-    print find_path(u"E:\\workspace",u"啊")
+    print find_path(u"E:\\workspace",u"pytests")
     
